910501d309f96dae795d7d72a49a611d7bd6e8cc
Rafal Fuz <rafalfuz@gmail.com>
2021-10-16 23:30:56 +0200

    ""

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 import MainTemplate from '../templates/MainTemplate'
 
+const StyledWrapper = styled.div`
+    position: absolute;
+    left: 50%;
+    top :50%;
+    transform: translate(-50%, -50%);
+    width: 90vw;
+    height: 80vh;
+    background-color: white;
+`
 const Root = () => (
     <MainTemplate>
-    <h1>Hi. I am Rafał </h1>
-    <h2>Here will be my website</h2>
+    <StyledWrapper>
+        <></>
+    </StyledWrapper>
     </MainTemplate>
 )
 export default Root
