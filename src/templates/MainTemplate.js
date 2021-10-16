a9913fd010f828dd878d724c371f224eaa74c66a
Rafal Fuz <rafalfuz@gmail.com>
2021-10-16 21:08:36 +0200

    Upload to gitHub

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {ThemeProvider} from 'styled-components'
+import GlobalStyle from '../theme/globalStyle'
+import {theme} from '../theme/mainTheme'
+
+
+const MainTemplate = ({children}) => (
+    <div>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    </div>
+)
+ 
+export default MainTemplate;
---
+++ src/views/Root.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainTemplate from '../templates/MainTemplate'
+
+const Root = () => (
+    <MainTemplate>
+    <h1>Hi. I am Rafał </h1>
+    <h2>Here will be my website</h2>
+    </MainTemplate>
+)
+export default Root
